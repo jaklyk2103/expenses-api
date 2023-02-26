e7183691d30bbe556089290d5080a77070b139aa
Jakub Lyko <kubeckk21@gmail.com>
2023-02-26 11:13:10 +0100

    Disabled etags for testing

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import serverless from 'serverless-http';
 import router from './router';
 
 const app = express();
+app.disable('etag');
 app.use(express.json());
 app.use((req: Request, res: Response, next: NextFunction ) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://expenses-8c745.web.app');
